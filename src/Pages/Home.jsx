9b6b2f40d7e81ee079296ddc761927c6e952b341
Abhinav kumar Bindra <abhinavbindra1258@gmail.com>
2026-05-02 11:13:47 +0530

    Banner section completed

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "../Components/Hero";
 import FeaturedSection from "../Components/featuredSection";
+import Banner from "../Components/Banner";
 
 const Home = () => {
   return (
     <>
       <Hero />
       <FeaturedSection />
+      <Banner />
     </>
   );
 };
